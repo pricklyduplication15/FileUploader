36ff90447a281df1bf33ff5d2d50c9d3e9fbe9d9
pricklyduplication15 <129458787+pricklyduplication15@users.noreply.github.com>
2024-05-03 13:12:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,56 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+var path = require("path");
+require("dotenv").config();
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
-
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/public", express.static(path.join(process.cwd(), "/public")));
+app.get("/", function (req, res) {
+  res.sendFile(path.join(process.cwd(), "/views/index.html"));
 });
 
+// Set storage engine
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(process.cwd(), "/public/uploads"));
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+    );
+  },
+});
 
+// Init multer
+var upload = multer({ storage: storage }).single("upfile");
 
+app.post("/api/fileanalyse", (req, res) => {
+  upload(req, res, function (err) {
+    if (err) {
+      return res.status(500).send(err);
+    }
+    if (!req.file) {
+      return res.status(400).send("No files were uploaded.");
+    }
+
+    const uploadedFile = req.file;
+    const fileInfo = {
+      name: uploadedFile.originalname,
+      type: uploadedFile.mimetype,
+      size: uploadedFile.size,
+    };
+
+    res.json(fileInfo);
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
